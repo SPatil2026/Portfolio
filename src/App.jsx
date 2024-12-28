482fbcd104f540b28fc6c7d28613a8687a29ff2e
Samarth Patil <samarthpatil2022.comp@mmcoe.edu.in>
2024-12-29 00:02:31 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import About from "./components/About"
+import BlurBackground from "./components/BlurBackground"
+import Contacts from "./components/Contacts"
+import Experience from "./components/Experience"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+
+const App = () => {
+  return (
+    <>
+      <BlurBackground />
+      <Navbar />
+      <main className="antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10">
+        <Navbar />
+        <Hero />
+        <Projects />
+        <About />
+        <Experience />
+        <Contacts />
+      </main>
+    </>
+  )
+}
+
+export default App
